158585ba1d0a9a47d0ec55cb064ceb30e07ff024
闫海亮 <1015951447@qq.com>
2025-04-21 14:02:33 +0800

    新建项目

---
+++ src/main/java/com/yhllx999/webdiaryboot/WebDiaryBootApplication.java
@@ -0,0 +1,13 @@
+package com.yhllx999.webdiaryboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDiaryBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDiaryBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yhllx999/webdiaryboot/WebDiaryBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhllx999.webdiaryboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDiaryBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
